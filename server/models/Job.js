9fa8ccbfb7a394269770b2eb21378e27063433bc
Aveneil Lucena <neillucena97@gmail.com>
2025-06-25 19:45:57 -0700

    Created Job model schema

---
+++ server/models/Job.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const JobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please provide job title'],
+    trim: true,
+  },
+  company: {
+    type: String,
+    required: [true, 'Please provide company name'],
+    trim: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'interview', 'declined', 'accepted'],
+    default: 'pending',
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  notes: {
+    type: String,
+    trim: true,
+    default: '',
+  },
+});
+
+module.exports = mongoose.model('Job', JobSchema);
